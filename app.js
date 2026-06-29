76cafc836bcb3aa67b81695fc9a9956cf92457b5
Bhavya Sri Baggi <111896968+Bhavyasri-Baggi@users.noreply.github.com>
2026-06-29 22:16:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from ECS Fargate!');
+  res.send('Hello from ECS Fargate! poc 24');
 });
 
 app.listen(8080, () => console.log('App running on port 8080'));
