94e33b99b69c62414451fd9fd683b0f5a61a13c4
Bhavyasri-Baggi <bhavyabaggi407@gmail.com>
2026-06-30 04:59:27 +0000

    changes made

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from ECS Fargate! poc 24 inaction in action' );
+  res.send('Hello from ECS Fargate! poc 24 inaction in action pika' );
 });
 
 app.listen(8080, () => console.log('App running on port 8080'));
